0a9d284e84525b8b5271b0dc5db4cfdb7aefb8bf
Scott Soderstrom <ssoderstrom@gmail.com>
2024-06-14 00:34:42 -1000

    Update main.py - fix

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import logging
-from fastapi import FastAPI, Request, HTTPException, Response
-from pydantic import BaseModel, Field
-from typing import List
-import json
-from pprint import pprint
-from rerankers import Reranker
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Any
+from rerankers import Reranker, Document
 from dotenv import load_dotenv
 import os
 
@@ -12,10 +9,6 @@ import os
 load_dotenv()
 
 app = FastAPI()
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("fastapi")
 
 # ranker = Reranker("cohere", lang='en', api_key='n1ytpDT5S9jVqY1abqvqoD6flMgo8M25UJce9fLy')
 # ranker = Reranker('mixedbread-ai/mxbai-rerank-large-v1', model_type='cross-encoder')
